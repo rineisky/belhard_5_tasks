2afe3b94eeda9560be0f643b991022fcad9c10ad
Vyacheslav Rineisky <rineisky@gmail.com>
2021-06-29 20:07:03 +0300

    added medium

---
+++ tasks/medium/pow_2.py
@@ -0,0 +1,17 @@
+"""
+Проверить, является ли число степенью двойки.
+Вернуть True или False
+Является ли число степенью 2
+"""
+
+
+def is_pow_2(number) -> bool:
+    return None
+
+
+if __name__ == '__main__':
+    assert is_pow_2(4)
+    assert is_pow_2(16)
+    assert is_pow_2(256)
+    assert not is_pow_2(123)
+    print("Решено")
---
+++ tasks/medium/range.py
@@ -0,0 +1,24 @@
+"""
+ЗАДАНИЕ
+--------------------------------------------------------------------------------
+Написать функцию list_compose, которая принимает два списка (INDEX_LIST, VALUE_LIST).
+Составить новый список: берем индекс из списка индексов, и вставляем значение по этому
+индексу из другого списка. Если значения нет, что вставить None
+
+ПРИМЕРЫ
+--------------------------------------------------------------------------------
+list_compose(INDEX_LIST, VALUE_LIST) -> ['b', 'f', None, None, 'c', 'd', None, 'e']
+"""
+INDEX_LIST = [1, -1, 6, -12, 2, 3, 9, 4]
+VALUE_LIST = ['a', 'b', 'c', 'd', 'e', 'f']
+
+
+def list_compose(indexes: list, values: list) -> list:
+    result_list = []
+    return result_list
+
+
+if __name__ == '__main__':
+    assert list_compose(INDEX_LIST, VALUE_LIST) == ['b', 'f', None, None, 'c', 'd',
+                                                    None, 'e']
+    print('Решено!')
